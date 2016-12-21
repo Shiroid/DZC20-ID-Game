92ff7d5c5cc41ecba3ec593fdb2d5cc5f41c3f56
Shiroid <l.w.p.h.v.gansewinkel@student.tue.nl>
2016-12-21 16:05:17 +0100

    Fixed menu-camera interaction and added script for camera following an object.

---
+++ Assets/Scripts/CameraLeader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLeader : MonoBehaviour
+{
+
+    public GameObject camera;
+
+    // Late update is called once per frame
+    void LateUpdate()
+    {
+        camera.transform.position = new Vector3(transform.position.x, transform.position.y, camera.transform.position.z);
+    }
+}
