6c9047c8608904f6c62f2f0088234237d91e60b2
TUE\s140665 <f.f.v.himbergen@student.tue.nl>
2016-12-22 03:57:14 +0100

    level erin, teleport werkt bijna gvd

---
+++ Assets/Scripts/TeleportController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportController : MonoBehaviour {
+
+	public GameObject objecttotp;
+	public Transform target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnTrigger(Collider other) {
+		if (other.gameObject.CompareTag ("Player")) {
+			objecttotp.gameObject.transform.position = target.transform.position;
+		}
+	}
+}
