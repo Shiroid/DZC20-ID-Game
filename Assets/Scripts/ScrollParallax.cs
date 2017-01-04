50b4f6c345990657855058232295c281d2688f1f
Shiroid <l.w.p.h.v.gansewinkel@student.tue.nl>
2017-01-05 00:39:59 +0100

    Added parallax scrolling script.

---
+++ Assets/Scripts/ScrollParallax.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollParallax : MonoBehaviour {
+
+    public GameObject cameraFocus;
+    public float distanceMultiplier;
+
+    private Vector3 basePosition;
+
+	// Use this for initialization
+	void Start () {
+        basePosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        Vector3 focusPos = cameraFocus.transform.position;
+        if(distanceMultiplier > 1)
+            transform.position = (basePosition + focusPos*(distanceMultiplier-1))/distanceMultiplier;
+	}
+}
